0a81b86315c9619cd693f1ffb45bde7641bc6942
zahraaAed <zahraaalaaeddine94@gmail.com>
2023-12-02 15:13:34 +0200

    registration

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -5,11 +5,13 @@ export default function Registration() {
   return (
     <>
       <Header />
-     <div className='mt-5'>
-      <h1 style={{marginLeft: '90px',color: '#273384'}} className='mb-5 text-start'>Welcome to PAYPLEX - Your Gateway to Secure Financial Management.</h1>
-<h3 style={{marginLeft: '90px', color: '#809BAA'}} className='mb-5 text-start'>Create your account today and unlock a world of personalized experiences! Please fill in the following information to get started.</h3>
+      <div className="Register">
+     <div className='mt-8 '>
+      <h1 style={{marginLeft: '90px',color: '#273384',paddingTop:'40px'}} className='mb-5 text-start'>Welcome to PAYPLEX - Your Gateway to Secure Financial Management.</h1>
+<h3 style={{marginLeft: '90px', color: '#809BAA'}} className='mb-5 text-start'>Create your account today and unlock a world of personalized experiences!<br/> Please fill in the following information to get started.</h3>
 </div>
       <Form />
+      </div>
     </>
   );
 }
