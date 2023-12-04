b24e22d792fed973049e43639adee2bfc7d691a4
zahraaAed <zahraaalaaeddine94@gmail.com>
2023-12-04 16:19:31 +0200

    the last version for admin

---
+++ src/pages/TransferPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import Search from "../components/Search";
+export default function Transfer(){
+    return(
+      <div className="transfer">
+        <Sidebar /> 
+  
+     <div className="container-search">
+        <Search/>
+     </div>
+        </div>
+    );
+}
+
+
+
+
