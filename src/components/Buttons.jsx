ff6105d56eca9e96e2ea1724112032e99b58e9ba
zahraaAed <zahraaalaaeddine94@gmail.com>
2023-12-08 09:14:33 +0200

    adding admin login

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -3,9 +3,9 @@ import { NavLink } from "react-router-dom";
 export default function Button() {
   return (
     <div className="my-3">
-      <button type="button" className="btn btn-primary btn-sm me-2">Deposit</button>
+      {/* <button type="button" className="btn btn-primary btn-sm me-2">Deposit</button> */}
       <NavLink exact to="/moneyTransfer" activeClassName="activeClicked" >
-      <button type="button" className="btn btn-secondary btn-sm">Transfer</button>
+      <button type="button" className="btn  btn-sm" style={{ marginLeft: '40px' , backgroundColor: '#ffd700', color: '#273384'}}>Transfer</button>
       </NavLink>
     </div>
   );
